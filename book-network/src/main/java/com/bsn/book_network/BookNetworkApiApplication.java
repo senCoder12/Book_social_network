b662017c0214f37a582d0b6352f4803515376fc8
mayukhSen12 <mayukhsen448@gmail.com>
2025-01-05 14:59:58 +0530

    Implemented Auditing

--- book-network/src/main/java/com/bsn/book_network/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/bsn/book_network/BookNetworkApiApplication.java
@@ -11,7 +11,7 @@ import com.bsn.book_network.role.Role;
 import com.bsn.book_network.role.RoleRepository;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @EnableAsync
 public class BookNetworkApiApplication {
 
