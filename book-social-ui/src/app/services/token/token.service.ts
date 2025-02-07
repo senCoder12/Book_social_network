44d1c9f2e7fc9340d7201b634992eb8916a902bd
senCoder12 <mayukhsen85082@gmail.com>
2025-02-08 02:22:37 +0530

    added auth guard

--- book-social-ui/src/app/services/token/token.service.ts
+++ book-social-ui/src/app/services/token/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,20 @@ export class TokenService {
 
   get token(): string {
     return localStorage.getItem('token') as string;
+  }
+
+  isTokenNotValid(): boolean {
+    return !this.isTokenValid();
+  }
+
+  private isTokenValid(): boolean {
+    const token: string = this.token;
+    if (!token) {
+      return false;
+    }
+
+    const jwtHelper = new JwtHelperService();
+    const isTokenExpired: boolean = jwtHelper.isTokenExpired(token);
+    return !isTokenExpired;
   }
 }
